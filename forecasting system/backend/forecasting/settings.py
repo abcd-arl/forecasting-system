54019ba8a2c533dff5578433e10ebe6ef3eca9bb
abcd-arl <lingga.abdulrahman.u@gmail.com>
2023-04-06 12:28:18 +0800

    change static settings

--- forecasting system/backend/forecasting/settings.py
+++ forecasting system/backend/forecasting/settings.py
@@ -143,8 +143,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
-STATICFILES_DIRS = [BASE_DIR / 'static']
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_DIRS = [BASE_DIR / 'static']
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 AWS_ACCESS_KEY_ID = env.str("AWS_ACCESS_KEY_ID")
