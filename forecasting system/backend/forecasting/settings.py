4a5b6d9e3e7f377017b1af2c375998e625f9eb58
abcd-arl <lingga.abdulrahman.u@gmail.com>
2023-04-06 12:45:39 +0800

    update settings

--- forecasting system/backend/forecasting/settings.py
+++ forecasting system/backend/forecasting/settings.py
@@ -88,16 +88,16 @@ WSGI_APPLICATION = 'forecasting.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-# DATABASES = {
-#   'default': {
-#     'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#     'NAME': env.str("DB_NAME"),
-#     'USER': env.str("DB_USER"),
-#     'PASSWORD': env.str("DB_PASSWORD"),
-#     'HOST': env.str("DB_HOST"),
-#     'PORT': env.str("DB_PORT"),
-#   }
-# }
+DATABASES = {
+  'default': {
+    'ENGINE': 'django.db.backends.postgresql_psycopg2',
+    'NAME': env.str("DB_NAME"),
+    'USER': env.str("DB_USER"),
+    'PASSWORD': env.str("DB_PASSWORD"),
+    'HOST': env.str("DB_HOST"),
+    'PORT': env.str("DB_PORT"),
+  }
+}
 
 DATABASES = {
     'default': {
